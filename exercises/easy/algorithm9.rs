69847daa7e61a71175f4936091cc2c9dc3d9b65d
苹果喵喵 <149125378+PingGuoMiaoMiao@users.noreply.github.com>
2025-02-20 20:31:18 +0800

    Update algorithm9.rs

--- exercises/easy/algorithm9.rs
+++ exercises/easy/algorithm9.rs
@@ -1,9 +1,3 @@
-/*
-	heap
-	This question requires you to implement a binary heap function
-*/
-
-
 use std::cmp::Ord;
 use std::default::Default;
 
@@ -23,7 +17,7 @@ where
     pub fn new(comparator: fn(&T, &T) -> bool) -> Self {
         Self {
             count: 0,
-            items: vec![T::default()],
+            items: vec![T::default()], // 索引 0 不使用
             comparator,
         }
     }
@@ -37,15 +31,25 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.items.push(value); // 将新元素插入到数组末尾
+        self.count += 1;
+        self.swim(self.count); // 上浮操作
+    }
+
+    pub fn next(&mut self) -> Option<T> {
+        if self.is_empty() {
+            return None;
+        }
+        let root = self.items.swap_remove(1); // 移除堆顶元素
+        self.count -= 1;
+        if !self.is_empty() {
+            self.sink(1); // 下沉操作
+        }
+        Some(root)
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
         idx / 2
-    }
-
-    fn children_present(&self, idx: usize) -> bool {
-        self.left_child_idx(idx) <= self.count
     }
 
     fn left_child_idx(&self, idx: usize) -> usize {
@@ -56,9 +60,38 @@ where
         self.left_child_idx(idx) + 1
     }
 
-    fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+    fn swim(&mut self, mut idx: usize) {
+        while idx > 1 {
+            let parent_idx = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent_idx]) {
+                self.items.swap(idx, parent_idx);
+                idx = parent_idx;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn sink(&mut self, mut idx: usize) {
+        while self.left_child_idx(idx) <= self.count {
+            let left = self.left_child_idx(idx);
+            let right = self.right_child_idx(idx);
+            let mut smallest = idx;
+
+            if left <= self.count && (self.comparator)(&self.items[left], &self.items[smallest]) {
+                smallest = left;
+            }
+            if right <= self.count && (self.comparator)(&self.items[right], &self.items[smallest]) {
+                smallest = right;
+            }
+
+            if smallest != idx {
+                self.items.swap(idx, smallest);
+                idx = smallest;
+            } else {
+                break;
+            }
+        }
     }
 }
 
@@ -66,12 +99,12 @@ impl<T> Heap<T>
 where
     T: Default + Ord,
 {
-    /// Create a new MinHeap
+    /// 创建一个最小堆
     pub fn new_min() -> Self {
         Self::new(|a, b| a < b)
     }
 
-    /// Create a new MaxHeap
+    /// 创建一个最大堆
     pub fn new_max() -> Self {
         Self::new(|a, b| a > b)
     }
@@ -84,8 +117,7 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        self.next()
     }
 }
 
@@ -97,7 +129,7 @@ impl MinHeap {
     where
         T: Default + Ord,
     {
-        Heap::new(|a, b| a < b)
+        Heap::new_min()
     }
 }
 
@@ -109,13 +141,14 @@ impl MaxHeap {
     where
         T: Default + Ord,
     {
-        Heap::new(|a, b| a > b)
+        Heap::new_max()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_empty_heap() {
         let mut heap = MaxHeap::new::<i32>();
