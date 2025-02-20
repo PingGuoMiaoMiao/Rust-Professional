11914a2a24bae0c3761c53654bc66c45ae4b67c3
苹果喵喵 <149125378+PingGuoMiaoMiao@users.noreply.github.com>
2025-02-20 20:46:27 +0800

    Update fibnacci.rs

--- exercises/normal/solution5/src/fibnacci.rs
+++ exercises/normal/solution5/src/fibnacci.rs
@@ -1,4 +1,17 @@
+// src/fibnacci.rs
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    let mut a = 0;
+    let mut b = 1;
+    let mut sum = 0;
+ 
+    while a <= threshold {
+        if a % 2 != 0 {
+            sum += a;
+        }
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+ 
+    sum
 }
