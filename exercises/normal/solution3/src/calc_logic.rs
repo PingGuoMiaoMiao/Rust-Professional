bc0bf830687610fc52900cf3941ca415e662e511
苹果喵喵 <149125378+PingGuoMiaoMiao@users.noreply.github.com>
2025-02-20 20:45:34 +0800

    Update calc_logic.rs

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,15 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
-    // TODO: 这里写逻辑
-    todo!()
+    if n == 0 {
+        return 0.0;
+    }
+    if n > 365 {
+        return 1.0;
+    }
+
+    let mut probability = 1.0;
+    for k in 1..n {
+        probability *= 1.0 - (k as f64) / 365.0;
+    }
+
+    1.0 - probability
 }
